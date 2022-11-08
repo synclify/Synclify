0215ed75eb92bf2e720314ffaee6fba04aca2f49
Andrea De Masi <andreademasi80@gmail.com>
2022-11-08 13:34:28 +0100

    Initial repo setup

--- src/contents/plasmo.ts
+++ src/contents/plasmo.ts
@@ -1,11 +1,91 @@
-import type { PlasmoContentScript } from "plasmo"
+import { Storage } from "@plasmohq/storage"
+import { io } from "socket.io-client"
 
-export const config: PlasmoContentScript = {
-  matches: ["https://www.plasmo.com/*"]
+// TODO: create socket events and storage states types
+
+const storage = new Storage({ area: "session" })
+
+const socket = io(
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "socket-andreademasi.koyeb.app"
+)
+
+console.log("loaded")
+
+let room: string
+
+storage.watch({
+  room: (r) => {
+    room = r.newValue
+    if (room) {
+      console.log("Joining room ", room)
+      socket.emit("join", room)
+    }
+  }
+})
+
+socket.on("full", (room) => {
+  // TODO: Handle full room
+  console.log("Room " + room + " is full")
+})
+
+socket.on("join", (room) => {
+  console.log("Making request to join room " + room)
+})
+
+socket.on("log", (array) => {
+  console.log.apply(console, array)
+})
+
+socket.onAny((eventName, ...args) => {
+  console.log("Event: ", eventName, ...args)
+})
+
+socket.on("message", (message) => {
+  console.log("New message: " + message)
+  let element = getElementByXPath(message)
+  console.log("element is: ", element)
+  element.click()
+})
+
+document.addEventListener("click", (ev) => {
+  if (ev.isTrusted && room) {
+    console.log(ev.target)
+    let t = ev.target
+    let path = getXPathForElement(t as HTMLElement)
+    console.log(path)
+    console.log(t === getElementByXPath(path))
+    socket.emit("message", room, path)
+  }
+})
+
+function getXPathForElement(element: HTMLElement): string {
+  const idx = (
+    sib: { previousElementSibling; localName: any },
+    name?: undefined
+  ) =>
+    sib
+      ? idx(sib.previousElementSibling, name || sib.localName) +
+        (sib.localName == name)
+      : 1
+  const segs = (elm) =>
+    !elm || elm.nodeType !== 1
+      ? [""]
+      : elm.id && document.getElementById(elm.id) === elm
+      ? [`id("${elm.id}")`]
+      : [...segs(elm.parentNode), `${elm.localName.toLowerCase()}[${idx(elm)}]`]
+  return segs(element).join("/")
 }
 
-window.addEventListener("load", () => {
-  console.log("content script loaded")
+function getElementByXPath(path: string) {
+  return new XPathEvaluator().evaluate(
+    path,
+    document.documentElement,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue as HTMLElement
+}
 
-  document.body.style.background = "pink"
-})
+export {}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,96 @@
-import { useState } from "react"
+import React, { useCallback, useEffect } from "react"
+
+import { io } from "socket.io-client"
+import { useForm } from "react-hook-form"
+import { useStorage } from "@plasmohq/storage/hook"
+
+const socket = io("http://localhost:3000")
+
+type FormData = {
+  room: string
+}
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [room, , { setRenderValue, setStoreValue, remove }] = useStorage({
+    key: "room",
+    area: "session"
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const createRoom = useCallback(() => {
+    socket.emit("create")
+  }, [])
+
+  const joinRoom = useCallback((data: FormData) => {
+    const room = data.room.toUpperCase()
+    setRenderValue(room)
+    setStoreValue(room)
+  }, [])
+
+  useEffect(() => {
+    socket.on("create", (room) => {
+      console.log("code created by server", room)
+      setStoreValue(room)
+    })
+
+    return () => {
+      socket.off("create")
+    }
+  }, [])
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-    </div>
+    <React.StrictMode>
+      {room ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: 16
+          }}>
+          <h1>Room code: {room}</h1>
+          <button onClick={() => remove()}>Exit</button>
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: 16
+          }}>
+          <button onClick={createRoom}>Create room</button>
+          <p>or</p>
+          <p>Join room: </p>
+          <form onSubmit={handleSubmit(joinRoom)}>
+            <input
+              type="text"
+              placeholder="Room code"
+              {...register("room", {
+                required: { value: true, message: "Room code can't be empty." },
+                maxLength: { value: 5, message: "Room code too long." },
+                minLength: { value: 5, message: "Room code too short" },
+                pattern: {
+                  value: /^[a-zA-Z0-9]*$/,
+                  message: "Room code format incorrect"
+                }
+              })}
+            />
+            {errors.room && (
+              <div>
+                <p style={{ color: "red" }} role="alert">
+                  {errors.room?.message}
+                </p>
+              </div>
+            )}
+            <input type="submit" value="Join!" />
+          </form>
+        </div>
+      )}
+    </React.StrictMode>
   )
 }
 
