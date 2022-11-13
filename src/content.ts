93e3219db1099a1fb0c646e9cdeca88a8196002c
Andrea De Masi <andreademasi80@gmail.com>
2022-11-13 17:25:09 +0100

    Main logic refactor

--- src/content.ts
+++ src/content.ts
@@ -2,26 +2,51 @@ import { SOCKET_EVENTS } from "~types/socket"
 import { Storage } from "@plasmohq/storage"
 import { VIDEO_EVENTS } from "~types/video"
 import { io } from "socket.io-client"
+import { parseRooms } from "~utils/rooms"
 
-export {}
+console.log("loaded")
 
-const storage = new Storage({ area: "session" })
-
+let room
+let video: HTMLVideoElement
+const storage = new Storage({ area: "local" })
 const socket = io(
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : process.env.PLASMO_PUBLIC_SOCKET_ENDPOINT
 )
 
-console.log("loaded")
+const getCookie = (cookie: string) => {
+  // https://stackoverflow.com/a/19971550/934239
+  return document.cookie.split(";").reduce(function (prev, c) {
+    const arr = c.split("=")
+    return arr[0].trim() === cookie ? arr[1] : prev
+  }, undefined)
+}
 
-let room: string
-let video: HTMLVideoElement
+const tabId = getCookie("tab_id")
+console.log(tabId)
+
+storage.get("rooms").then((r) => {
+  console.log("getting rooms: ", r)
+
+  if (r) {
+    const rooms = parseRooms(r)
+    console.log(rooms)
+    if (rooms[tabId]) {
+      room = rooms[tabId]
+      console.log("Joining room ", room)
+      socket.emit(SOCKET_EVENTS.JOIN, room)
+    }
+  }
+})
 
 storage.watch({
-  room: (r) => {
-    room = r.newValue
-    if (room) {
+  rooms: (r) => {
+    const rooms = parseRooms(r.newValue)
+    console.log(rooms)
+    room = rooms[tabId]
+    if (rooms[tabId]) {
+      room = rooms[tabId]
       console.log("Joining room ", room)
       socket.emit(SOCKET_EVENTS.JOIN, room)
     }
@@ -42,30 +67,27 @@ socket.on(SOCKET_EVENTS.LOG, (array) => {
 })
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  )
   console.log("request: ", request)
   if (request.message === "detectVideo") {
-    document.addEventListener("click", handleVideoDetect)
-    sendResponse({ message: "ok" })
-    return true
+    const videos = document.getElementsByTagName("video")
+    if (videos.length === 0)
+      sendResponse({ status: "error", message: "No videos found" })
+    else {
+      video = videos[0]
+      Object.values(VIDEO_EVENTS).forEach((event) =>
+        video.addEventListener(event, (e) => videoEventHandler(e))
+      )
+      sendResponse({ status: "success", message: "ok" })
+    }
   }
-  sendResponse({ message: "bad" })
   return true
 })
 
-const handleVideoDetect = (ev) => {
+const handleVideoDetectManually = (ev) => {
   video = ev.target.closest("video") as HTMLVideoElement
   console.log(video)
   if (video != null) {
-    Object.values(VIDEO_EVENTS).forEach((event) =>
-      video.addEventListener(event, (e) => videoEventHandler(e))
-    )
-
-    document.removeEventListener("click", handleVideoDetect)
+    document.removeEventListener("click", handleVideoDetectManually)
   }
 }
 
@@ -84,6 +106,7 @@ const videoEventHandler = (event: Event) => {
 socket.on(
   SOCKET_EVENTS.VIDEO_EVENT,
   (eventType: VIDEO_EVENTS, volumeValue: string, currentTime: string) => {
+    const time = Number.parseInt(currentTime)
     switch (eventType) {
       case VIDEO_EVENTS.PLAY:
         video.play()
@@ -95,38 +118,11 @@ socket.on(
         video.volume = Number.parseInt(volumeValue)
         break
       case VIDEO_EVENTS.SEEKED:
-        video.currentTime = Number.parseInt(currentTime)
+        // this check avoids entering in a loop between the two clients, a better solution could be found
+        if (Math.abs(video.currentTime - time) > 1 && !video.seeking)
+          video.currentTime = time
     }
   }
 )
 
-/*
-
-function getXPathForElement(element: HTMLElement): string {
-  const idx = (
-    sib: { previousElementSibling; localName: any },
-    name?: undefined
-  ) =>
-    sib
-      ? idx(sib.previousElementSibling, name || sib.localName) +
-        (sib.localName == name)
-      : 1
-  const segs = (elm) =>
-    !elm || elm.nodeType !== 1
-      ? [""]
-      : elm.id && document.getElementById(elm.id) === elm
-      ? [`id("${elm.id}")`]
-      : [...segs(elm.parentNode), `${elm.localName.toLowerCase()}[${idx(elm)}]`]
-  return segs(element).join("/")
-}
-
-function getElementByXPath(path: string) {
-  return new XPathEvaluator().evaluate(
-    path,
-    document.documentElement,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  ).singleNodeValue as HTMLElement
-}
-*/
+export {}
