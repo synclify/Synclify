b812c8756baf8a00f18503d33128f21413de4eca
Andrea De Masi <andreademasi80@gmail.com>
2022-11-16 20:13:36 +0100

    Improved tab id sharing

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,18 @@
-export {}
+import { createChromeHandler } from "trpc-chrome/adapter"
+import { initTRPC } from "@trpc/server"
 
-console.log("loaded")
-
-// stores tab id in a cookie so it can be accessed SYNCHRONOUSLY from content script
-// if users opens two tabs on the same website, the same cookie is used which leads to the same room code used
-// TODO: find a better solution
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (
-    changeInfo.url &&
-    changeInfo.url != "chrome://newtab/" &&
-    changeInfo.url != "chrome://extensions/"
-  ) {
-    console.log(tabId)
-    chrome.cookies.set({
-      name: "tab_id",
-      value: tabId.toString(),
-      url: changeInfo.url
-    })
-  }
+const t = initTRPC.create({
+  isServer: false,
+  allowOutsideOfServer: true
 })
+
+const appRouter = t.router({
+  getTabId: t.procedure.query(async () => {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    return tabs[0].id
+  })
+})
+
+export type AppRouter = typeof appRouter
+console.log("loaded")
+createChromeHandler({ router: appRouter /* 👈 */ })
