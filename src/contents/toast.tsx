6e9fd9dd4f8ebbbae0b711fa7dfe2f23694cc4f4
Andrea De Masi <andreademasi80@gmail.com>
2022-12-14 16:35:56 +0100

    Toast CSUI setup

--- src/contents/toast.tsx
+++ src/contents/toast.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type { PlasmoGetStyle, PlasmoRender } from "plasmo"
 import { useEffect, useState } from "react"
 
-import type { PlasmoGetStyle } from "plasmo"
 import browser from "webextension-polyfill"
-import icon from "data-base64:~assets/icon.png"
+import { createRoot } from "react-dom/client"
 import styleText from "data-text:../style.css"
 
 export const getStyle: PlasmoGetStyle = () => {
@@ -15,45 +14,42 @@ export const getStyle: PlasmoGetStyle = () => {
 const PlasmoOverlay = () => {
   const [show, setShow] = useState(false)
   const [content, setContent] = useState("")
-  const [error, setError] = useState(false)
-
   useEffect(() => {
-    const callback = (msg: {
-      to: string
-      show: boolean
-      content: string
-      error: boolean
-    }) => {
+    browser.runtime.onMessage.addListener((msg) => {
+      console.log(msg)
       if (msg.to === "toast") {
         if (msg.show) {
-          setError(msg.error)
+          setShow(true)
           setContent(msg.content)
-          setShow(msg.show)
-          setTimeout(() => setShow(false), 2000)
         } else setShow(false)
-        return false
+        return Promise.resolve(true)
       }
-    }
-    // @ts-ignore
-    browser.runtime.onMessage.addListener(callback)
+    })
 
-    return () => {
-      // @ts-ignore
-      browser.runtime.onMessage.removeListener(callback)
-    }
+    return () => {}
   }, [])
 
   return (
-    <div
-      className={`fixed right-0 flex translate-x-40 rounded-l-2xl border-y border-l bg-opacity-20 p-3 opacity-0 backdrop-blur transition duration-300 ${
-        show ? "translate-x-0 opacity-100" : ""
-      } ${
-        error ? "border-red-500 bg-red-500" : "border-green-400 bg-green-400"
-      }`}>
-      <img src={icon} alt="openW2G icon" className="mr-2 h-6 w-6" />
-      <p className="font-bold text-white">{content}</p>
-    </div>
+    <>
+      {show ? <span className="fixed bg-amber-100 p-3">{content}</span> : null}
+    </>
   )
+}
+export const render: PlasmoRender = async (
+  {
+    anchor = { element: document.body, type: "overlay" }, // the observed anchor, OR document.body.
+    createRootContainer // This creates the default root container
+  },
+  _,
+  OverlayCSUIContainer
+) => {
+  if (createRootContainer && anchor) {
+    const rootContainer = await createRootContainer(anchor)
+    console.log("called render")
+
+    const root = createRoot(rootContainer) // Any root
+    root.render(<PlasmoOverlay />)
+  }
 }
 
 export default PlasmoOverlay
